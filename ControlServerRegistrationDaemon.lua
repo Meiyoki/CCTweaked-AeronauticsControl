448d12cb3f811f53515441f4df6e834c65a686ff
Meiyoki <38023001+Meiyoki@users.noreply.github.com>
2026-07-08 02:49:13 +0800

    Update ControlServerRegistrationDaemon.lua

--- ControlServerRegistrationDaemon.lua
+++ ControlServerRegistrationDaemon.lua
@@ -2,26 +2,19 @@ local wirelessNIC = peripheral.find("modem", function(name, modem)
     return modem.isWireless()
 end)
 wirelessNIC.open(2000)
-settings.define("registrations.thrusters", {
-    description = "Thruster numbers for each grouping",
-    default = 
-    {
-      "fore" = 0,
-      "starboard" = 0,
-      "aft" = 0,
-      "port" = 0,
-      "vertical" = 0,
-    },
-    type = "table",
-})
 local components = 
 {
-  "fore" = 0,
-  "starboard" = 0,
-  "aft" = 0,
-  "port" = 0,
-  "vertical" = 0,
+  ["fore"] = 0,
+  ["starboard"] = 0,
+  ["aft"] = 0,
+  ["port"] = 0,
+  ["vertical"] = 0,
 }
+settings.define("registrations.thrusters", {
+    description = "Thruster numbers for each grouping",
+    default = components,
+    type = "table",
+})
 
 while true do
   eventData = os.pullEvent("modem_message")
